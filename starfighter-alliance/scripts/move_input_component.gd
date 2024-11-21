1532e27d795a4eca54b8e1886a2cdf740253a6c6
Alhasan Ramadan <alhasan.ramadan@uni-ulm.de>
2024-11-21 12:58:40 +0000

    Resolve "Raumschiff-Steuerung: Konstante Geschwindigkeit"

--- starfighter-alliance/scripts/move_input_component.gd
+++ starfighter-alliance/scripts/move_input_component.gd
@@ -6,4 +6,4 @@ extends Node
 func _input(event: InputEvent) -> void:
 	var input_axis_y = Input.get_axis("moveLeft", "moveRight")
 	var input_axis_x = Input.get_axis("moveDown", "moveUp")
-	move_component.velocity = Vector2(input_axis_y * 20, input_axis_x * -20) #Wir verstärken die Länge des Vektors in die Richtung
+	move_component.velocity = Vector2(input_axis_y * 300, input_axis_x * -300) #Wir verstärken die Länge des Vektors in die Richtung
