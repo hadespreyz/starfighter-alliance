70d448b9a334224b72fe28e4dac802cb8e0d10cd
Hasan <152815143+hadespreyz@users.noreply.github.com>
2025-01-13 17:38:07 +0100

    restored failed files

--- starfighter-alliance/scripts/x_wing.gd
+++ starfighter-alliance/scripts/x_wing.gd
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4971eb65d27d0d8d7303e78efc5f74b1c77e1806ed04bf9a658d571eb1ecffd8
-size 1407
+oid sha256:9e3ac75a61e3d26998129c72a10b3fd3e268fe65e20bdb7dff40f412a1b06cef
+size 6087
